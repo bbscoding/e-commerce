f2f680f8d6077fb68f49969230e3a44cf3c8723d
bbscoding <batuhan5534@gmail.com>
2025-07-30 17:12:35 +0300

    Started and header done

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import PageContainer from "./container/PageContainer"
+import Header from "./components/Header"
+import './App.css'
+
+
+function App() {
+
+  return (
+    <>
+      <PageContainer>
+        <Header />
+      </PageContainer>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+import logo from '../images/logo.png'
+import '../css/Header.css'
+import { CiShoppingBasket } from "react-icons/ci";
+import { MdOutlineLightMode } from "react-icons/md";
+import { MdLightMode } from "react-icons/md";
+
+
+
+
+function Header() {
+    const [darkTheme, setDarkTheme] = useState(false)
+    const changeDarkTheme = () => {
+        const root = document.querySelector("#root")
+        if (darkTheme) {
+            root.style.background = "#fff"
+            root.style.color = "black"
+        } else {
+            root.style.background = "black"
+            root.style.color = "#fff"
+        }
+        setDarkTheme(!darkTheme)
+    }
+    return (
+        <div className='header'>
+            <div className='flex-row'>
+                <img className='logo' src={logo} alt='logo' />
+                <p className='company-text'>Batu A.Ş</p>
+            </div>
+            <div className='flex-row'>
+                <input className='search-input' type="text" placeholder='Search...' />
+                <div>
+                    {
+                        darkTheme ? <MdOutlineLightMode className='icon' onClick={changeDarkTheme} /> : <MdLightMode className='icon' onClick={changeDarkTheme} />
+                    }
+
+                    <CiShoppingBasket className='icon' />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
